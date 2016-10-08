0e72abe75f25c37a8622e515f0a97ef62f626564
Greene1337 <Greeneegl@gmail.com>
2016-10-09 00:30:35 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "integer_list.hpp"
+using namespace std;
+int main()
+{
+	IntegerListInit();
+
+	IntegerListAccumulate();
+
+	IntegerListSplice();
+
+	IntegerListIsSorted();
+
+	IntegerListDestroy();
+}
