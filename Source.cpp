3e46fc207e9371dc861b4ae704f3de4a7b7c8507
Greene1337 <Greeneegl@gmail.com>
2016-11-25 19:02:12 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
+#include "integer_list_stack.hpp"
 #include "integer_list.hpp"
-using namespace std;
+#include <cassert>
+
 int main()
 {
-	IntegerListInit();
+	
+	IntegerListStack * pStack = IntegerListStackCreate();
 
-	IntegerListAccumulate();
+	
 
-	IntegerListSplice();
+	IntegerList l1, l2;
+	IntegerListInit(l1);
+	IntegerListInit(l2);
 
-	IntegerListIsSorted();
+	IntegerListPushBack(l1, 1);
+	IntegerListPushBack(l1, 2);
+	IntegerListPushBack(l1, 3);
 
-	IntegerListDestroy();
+	IntegerListPushBack(l2, 0);
+	IntegerListPushBack(l2, 2);
+	IntegerListPushBack(l2, 4);
+	IntegerListPushBack(l2, 6);
+
+		IntegerListStackPush(*pStack, l1);
+	IntegerListStackPush(*pStack, l2);
+
+		IntegerListClear(l1);
+	IntegerListClear(l2);
+
+		const IntegerList & lTop = IntegerListStackTop(*pStack);
+	assert(lTop.m_pLast->m_value == 6);
+
+	
+	IntegerListStackPop(*pStack);
+
+	
+	const IntegerList & lTop2 = IntegerListStackTop(*pStack);
+	assert(lTop2.m_pLast->m_value == 3);
+
+	
+	IntegerListStackDestroy(pStack);
+
+	return 0;
 }
